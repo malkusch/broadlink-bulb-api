72e42c86a7039cc0ab7881d4293279d1289ca083
Markus Malkusch <markus@malkusch.de>
2023-01-14 00:05:29 +0100

    Bump jakarta.xml.bind-api from 2.3.3 to 4.0.0

---
+++ src/main/java/javax/xml/bind/DatatypeConverter.java
@@ -0,0 +1,8 @@
+package javax.xml.bind;
+
+final public class DatatypeConverter {
+
+    public static String printHexBinary(byte[] val) {
+        return jakarta.xml.bind.DatatypeConverter.printHexBinary(val);
+    }
+}
